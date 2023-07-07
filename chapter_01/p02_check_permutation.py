78c34c73351ee923875258cf5a5c08531dc6f1e9
bartek.binda.misc <bartlomiej.binda@rwth-aachen.de>
2023-07-07 22:46:14 +0200

    add my solution for p02

--- chapter_01/p02_check_permutation.py
+++ chapter_01/p02_check_permutation.py
@@ -1,6 +1,21 @@
-# O(N)
+import time
 import unittest
-from collections import Counter
+from collections import defaultdict, Counter
+
+def is_permutation(string, permutation):
+    return sorted(string) == sorted(permutation)
+
+
+def is_permutation2(string, permutation):
+    dict_1 = {}
+    dict_2 = {}
+    
+    for letter in string:
+        dict_1[letter] = dict_1.get(letter, 0) + 1
+    for letter in permutation:
+        dict_2[letter] = dict_2.get(letter, 0) + 1
+    
+    return dict_1 == dict_2
 
 
 def check_permutation_by_sort(s1, s2):
@@ -52,16 +67,29 @@ class Test(unittest.TestCase):
     )
 
     testable_functions = [
+        is_permutation,
+        is_permutation2,
         check_permutation_by_sort,
         check_permutation_by_count,
         check_permutation_pythonic,
     ]
 
     def test_cp(self):
+        num_runs = 10000
+        function_runtimes = defaultdict(float)
+
         # true check
         for check_permutation in self.testable_functions:
             for str1, str2, expected in self.test_cases:
+                start = time.perf_counter()
                 assert check_permutation(str1, str2) == expected
+                function_runtimes[check_permutation.__name__] += (
+                        time.perf_counter() - start
+                    ) * 1000
+            
+        print(f"\n{num_runs} runs")
+        for function_name, runtime in function_runtimes.items():
+            print(f"{function_name}: {runtime:.1f}ms")
 
 
 if __name__ == "__main__":
