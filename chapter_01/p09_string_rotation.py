742646ac8a36e3deefa2fb7a317ec6c6ec7ce3fd
bartek.binda.misc <bartlomiej.binda@rwth-aachen.de>
2023-07-07 22:46:46 +0200

    add my solution for p09

--- chapter_01/p09_string_rotation.py
+++ chapter_01/p09_string_rotation.py
@@ -2,6 +2,14 @@
 import unittest
 
 
+def is_rotated_string(s1, s2):
+    """Checks if s2 is a rotation of s1"""
+    for i in range(len(s1)):
+        if s1[i:] + s1[:i] == s2:
+            return True
+    return False
+
+
 def string_rotation(s1, s2):
     if len(s1) == len(s2) != 0:
         return s2 in s1 * 2
@@ -19,6 +27,8 @@ class Test(unittest.TestCase):
     def test_string_rotation(self):
         for [s1, s2, expected] in self.test_cases:
             actual = string_rotation(s1, s2)
+            assert actual == expected
+            actual = is_rotated_string(s1, s2)
             assert actual == expected
 
 
