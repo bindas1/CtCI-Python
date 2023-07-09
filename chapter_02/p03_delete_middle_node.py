61a5e6367639cd7712d5fdb801a30a3ddf522d58
bartek.binda.misc <bartlomiej.binda@rwth-aachen.de>
2023-07-09 22:44:11 +0200

    my solution for 2.3

--- chapter_02/p03_delete_middle_node.py
+++ chapter_02/p03_delete_middle_node.py
@@ -1,6 +1,21 @@
 from chapter_02.linked_list import LinkedList
 
 
+def delete_middle(linked_list: LinkedList):
+    # check if len(list) < 3
+    if linked_list.head.next.next != None:
+        runner = linked_list.head
+        back = linked_list.head
+
+        while runner and runner.next:
+            runner = runner.next.next
+            back = back.next
+
+            if runner == None or runner.next == None:
+                back.value = back.next.value
+                back.next = back.next.next
+
+
 def delete_middle_node(node):
     node.value = node.next.value
     node.next = node.next.next
